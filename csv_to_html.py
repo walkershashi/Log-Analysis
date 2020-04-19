c0091142b8a29e585288ea70c51b366c8e7c195d
shashiwalker <skssunny30@gmail.com>
2020-04-19 21:36:28 +0530

    Created a python script 'csv_to_html.py' which takes the CSV file and converts them into a html table.

---
+++ csv_to_html.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+import pandas as pd
+
+#creating a list containing all the csv files passed
+files = sys.argv[1:]
+for file in files:
+    name = file.split('_')[0]
+    file = pd.read_csv(file) #converting the file into dataframe
+    html_file = file.to_html("{} Stats.html".format(name))
