e3ca0b4e8c48475258cfb03c6882331baf1f33cd
shashiwalker <skssunny30@gmail.com>
2020-07-13 23:04:11 +0530

    Added Comments

--- csv_to_html.py
+++ csv_to_html.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 #creating a list containing all the csv files passed
 files = sys.argv[1:]
+
 for file in files:
     name = file.split('_')[0]
     file = pd.read_csv(file) #converting the file into dataframe
-    html_file = file.to_html("{}_Stats.html".format(name))
+    html_file = file.to_html("{}_Stats.html".format(name)) # converting to html file
